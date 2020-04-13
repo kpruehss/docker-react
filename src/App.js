2be8bf823145e214c46719f923134e841a5a2dde
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2020-04-13 15:40:14 +1000

    Devel - First commit on dev branch

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to cool stuff happening?
-        </p>
+        <p>Phoenix, the Elixir of massive-scale WebDev</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://elixir-lang.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Elixir
         </a>
       </header>
     </div>
